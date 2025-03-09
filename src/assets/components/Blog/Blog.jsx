31f2984fb67b41cb86a64a8f474de205d82587a2
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-09 17:20:50 +0600

    mapping,load the data

--- src/assets/components/Blog/Blog.jsx
+++ src/assets/components/Blog/Blog.jsx
@@ -1,8 +1,13 @@
+import './Blog.css'
 const Blog = ({blog}) => {
-    console.log(blog)
+    const {cover,title,author,author_image} = blog
     return (
         <div>
-            
+            <img src={cover} alt="" srcset="" />
+            <h1 className='text-2xl'>{title}</h1>
+            <p>{author}</p>
+            <img className="author-img" src={author_image} alt="" srcset="" />
+            <br />
         </div>
     );
 };
--- src/assets/components/Blogs/Blogs.jsx
+++ src/assets/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs,setBlogs] = useState([])
@@ -12,6 +13,9 @@ const Blogs = () => {
     return (
         <div className=" md:w-2/3 ml-4">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog blog ={blog}></Blog>)
+            }
         </div>
     );
 };
