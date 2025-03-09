7a3f0f36d69ce2a0866c4875df7ce9c59d6b0c39
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-09 07:19:16 +0600

    header section

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import profile from '../../images/profile.jpg'
+import './Header.css'
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between  p-4 mx-4 border-spacing-y border-b-4'>
             <h1  className="text-3xl text-lime-800">Knowledge Cafe</h1>
+            <img className='profile-img' src={profile} alt="" srcset="" />
         </div>
     );
 };
