5d71051c0ee3776448446eb9a3ffe68901548329
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-09 15:11:57 +0600

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Blogs from './assets/components/blogs/blogs'
+import Bookmarks from './assets/components/Bookmarks/Bookmarks'
 import Header from './assets/components/header/header'
 
 
@@ -11,7 +12,12 @@ function App() {
   return (
     <>
       <Header></Header>
+
+      <div className='md:flex'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      
+      </div>
       
     </>
   )
--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import profile from '../../images/profile.jpg'
 import './Header.css'
 const Header = () => {
     return (
-        <div className='flex justify-between  p-4 mx-4 border-spacing-y border-b-4'>
+        <header className='flex justify-between  p-4 mx-4 border-spacing-y border-b-4'>
             <h1  className="text-3xl text-lime-800">Knowledge Cafe</h1>
             <img className='profile-img' src={profile} alt="" srcset="" />
-        </div>
+        </header>
     );
 };
 
